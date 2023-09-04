2deef29001ea776f9b45c78c3ec9f37da8bbb2c8
Nikhil <nikhillende9121@gmail.com>
2023-09-04 23:39:41 +0530

    v

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,60 +1,85 @@
-const API_URL = 'http://localhost:5000';
 
-const fetchOptions = {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  // You can add other options like credentials, headers, etc., here if needed
-};
 
-const processResponse = async (response) => {
-  if (response.status === 200) {
-    return {
-      isSuccess: true,
-      data: await response.json(),
-    };
-  } else {
-    const errorData = await response.json();
-    return {
-      isFailure: true,
-      status: response.status,
-      msg: errorData.msg,
-      code: errorData.code,
-    };
-  }
-};
+import axios from 'axios';
 
-const processError = (error) => {
-  console.error('ERROR: ', error);
-  return {
-    isError: true,
-    code: '',
-  };
-};
 
-// Signup API
-export const Signupuser = async (data) => {
-  try {
-    const response = await fetch(`${API_URL}/signup`, {
-      ...fetchOptions,
-      body: JSON.stringify(data),
-    });
-    return await processResponse(response);
-  } catch (error) {
-    return processError(error);
-  }
-};
+const API_URL = 'https://localhost:5000';
 
-// Login API
-export const Loginuser = async (data) => {
-  try {
-    const response = await fetch(`${API_URL}/login`, {
-      ...fetchOptions,
-      body: JSON.stringify(data),
-    });
-    return await processResponse(response);
-  } catch (error) {
-    return processError(error);
-  }
-};
+const axiosInstance = axios.create({
+    baseURL: API_URL,
+    timeout: 10000, 
+    headers: {
+        "content-type": "application/json"
+    }
+});
+
+axiosInstance.interceptors.request.use(
+    function(config) {
+        return config;
+    },
+    function(error) {
+        return Promise.reject(error);
+    }
+);
+
+axiosInstance.interceptors.response.use(
+    function(response) {
+        // Stop global loader here
+        return processResponse(response);
+    },
+    function(error) {
+        // Stop global loader here
+        return Promise.reject(ProcessError(error));
+    }
+)
+
+const processResponse = (response) => {
+    if (response?.status === 200) {
+        return { isSuccess: true, 
+            data: response.data }
+    } else {
+        return {
+            isFailure: true,
+            status: response?.status,
+            msg: response?.msg,
+            code: response?.code
+        }
+    }
+}
+
+const ProcessError = async (error) => {
+    if (error.response) {
+            console.log("ERROR IN RESPONSE: ", error);
+            return {
+                isError: true,
+                code: error.response.status
+            }
+        }
+    else if (error.request) { 
+        // The request was made but no response was received
+        console.log("ERROR IN RESPONSE: ", error);
+        return {
+            isError: true,
+            code: ""
+        }
+    } else { 
+        // Something happened in setting up the request that triggered an Error
+        console.log("ERROR IN RESPONSE: ", error);
+        return {
+            isError: true,
+            code: ""
+        }
+    }
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//singup API
+export const Signupuser=async(data)=>{
+        return await axiosInstance.post('/signup',data)
+    }
+
+// Login APi
+export const Loginuser=async(data)=>{
+    return await axiosInstance.post('/login',data)
+}
